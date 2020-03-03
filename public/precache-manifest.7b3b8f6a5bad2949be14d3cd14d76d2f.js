8610898916d8797dbe142036f769eaf7195e811f
neilkapadia7 <neilkapadia7@gmail.com>
2020-03-03 23:50:17 +0530

    Finished React GraphQL

---
+++ public/precache-manifest.7b3b8f6a5bad2949be14d3cd14d76d2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "605137ae2ac8cea976029f14609a2eb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d676a8e4adfe4facfc31",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "ab48e2a89c07fda27c98",
+    "url": "/static/js/2.82ee72d7.chunk.js"
+  },
+  {
+    "revision": "f8ad822fcd5ade052205fc2875fdd1c3",
+    "url": "/static/js/2.82ee72d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d676a8e4adfe4facfc31",
+    "url": "/static/js/main.174beae4.chunk.js"
+  },
+  {
+    "revision": "b681ecfa501425697523",
+    "url": "/static/js/runtime-main.33200e42.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
